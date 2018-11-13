1ad1d3cd7167b18aa095db2756a4c5e970a61020
Kehlin Swain <kehlinswain@gmail.com>
2018-11-12 20:44:06 -0600

    Update deviceInfo.js

--- deviceInfo.js
+++ deviceInfo.js
@@ -4,7 +4,7 @@ function deviceInfo_connect() {
         ({
             acceptAllDevices: true
             filters: [{
-                services: ['b42f7e37-b0ab-d1a3-1f76-2137c7b15671']
+                services: ['0x180F','b42f7e37-b0ab-d1a3-1f76-2137c7b15671']
          }]
         })
         .then(device => {
