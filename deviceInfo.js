60f486dcd700c6e9520105566f952c407db361a2
Kehlin Swain <kehlinswain@gmail.com>
2018-11-13 11:52:42 -0600

    Update deviceInfo.js

--- deviceInfo.js
+++ deviceInfo.js
@@ -2,7 +2,7 @@ function deviceInfo_connect() {
 
     navigator.bluetooth.requestDevice
         ({
-            acceptAllDevices: true
+            acceptAllDevices: true,
             filters: [{
                 services: ['0x00','e5f49879-6ee1-479e-bfec-3d35e13d3b88']
          }]
