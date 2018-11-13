7fff69904c4475ab86da5d49ea0a520eab9e3191
Kehlin Swain <kehlinswain@gmail.com>
2018-11-12 20:40:23 -0600

    update deviceinfo.js added uuid of flexdot emg

--- deviceInfo.js
+++ deviceInfo.js
@@ -3,6 +3,9 @@ function deviceInfo_connect() {
     navigator.bluetooth.requestDevice
         ({
             acceptAllDevices: true
+            filters: [{
+                services: ['b42f7e37-b0ab-d1a3-1f76-2137c7b15671']
+         }]
         })
         .then(device => {
             log('---------');
