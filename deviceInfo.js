09b18978a0c4a7299a6d5183194e7b898e61d810
Kehlin Swain <kehlinswain@gmail.com>
2018-12-10 19:40:13 -0600

    Update deviceInfo.js

--- deviceInfo.js
+++ deviceInfo.js
@@ -5,7 +5,7 @@ function deviceInfo_connect() {
             acceptAllDevices: true,
             filters: [{
                 services: ['0x00','e5f49879-6ee1-479e-bfec-3d35e13d3b88']
-         }]
+         }],
         })
         .then(device => {
             log('---------');
